2cf690b61b153a36a51f952e9308108be83f303c
Tiger Kaovilai <tkaovila@redhat.com>
2021-08-20 03:11:38 -0400

    noobaa is awesome

--- controllers/noobaa.go
+++ controllers/noobaa.go
@@ -1,15 +1,69 @@
 package controllers
 
 import (
+	// uncomment to use
+	// "github.com/noobaa/noobaa-operator/v2/pkg/apis/noobaa/v1alpha1"
+
 	"fmt"
-	noobaa "github.com/noobaa/noobaa-operator/v2/pkg/apis/noobaa/v1alpha1"
+
 	"github.com/go-logr/logr"
-	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
+	noobaa "github.com/noobaa/noobaa-operator/v5/pkg/apis/noobaa/v1alpha1"
 	oadpv1alpha1 "github.com/openshift/oadp-operator/api/v1alpha1"
+	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
+
+const (
+	apiVersion = "noobaa.io/v1alpha1"
+	nooBaaKind = "NooBaa"
+)
+
+var (
+	expected_crd_names = []string{
+		"noobaas.noobaa.io",
+		"bucketclasses.noobaa.io",
+		"backingstores.noobaa.io",
+	}
+	noobaa_labels = map[string]string{
+		"app": "noobaa",
+	}
+	deletePropagationForeground = metav1.DeletePropagationForeground
+)
+
+// func noobaaCrdObjectMeta() metav1.ObjectMeta {
+// 	return metav1.ObjectMeta{
+// 			Name: ,
+// 		}
+// }
+
+func (r *VeleroReconciler) getNoobaaObjectMeta() metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name:      "noobaa",
+		Namespace: r.NamespacedName.Namespace,
+	}
+}
+
+func (r *VeleroReconciler) getNoobaaBSL() velerov1.BackupStorageLocation {
+	return velerov1.BackupStorageLocation{
+		ObjectMeta: r.getNoobaaObjectMeta(),
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "BackupStorageLocation",
+			APIVersion: "velero.io/v1",
+		},
+	}
+}
+
+func (r *VeleroReconciler) getOadpOwnedNoobaaSystem() noobaa.NooBaa {
+	return noobaa.NooBaa{
+		ObjectMeta: r.getNoobaaObjectMeta(),
+		TypeMeta: metav1.TypeMeta{
+			Kind:       nooBaaKind,
+			APIVersion: apiVersion,
+		},
+	}
+}
 
 func (r *VeleroReconciler) ValidateNoobaa(log logr.Logger) (bool, error) {
 	velero := oadpv1alpha1.Velero{}
@@ -25,14 +79,12 @@ func (r *VeleroReconciler) ValidateNoobaa(log logr.Logger) (bool, error) {
 
 		//check no vsl or bsl are present
 
-		//default plugins should only consist of aws plugin	
+		//default plugins should only consist of aws plugin
 
 		//enable_restic:true flag present
 
-		//check if ocs and noobaa is up and running 
+		//check if ocs and noobaa is up and running
 	}
-
-
 	return true, nil
 }
 
@@ -48,7 +100,6 @@ func (r *VeleroReconciler) ReconcileNoobaa(log logr.Logger) (bool, error) {
 		return false, err
 	}
 
-
 	//Reconcile logic for Noobaa
 
 	//check if noobaa:true flag is present, if present proceed
@@ -58,20 +109,17 @@ func (r *VeleroReconciler) ReconcileNoobaa(log logr.Logger) (bool, error) {
 		bsl := velerov1.BackupStorageLocation{
 			ObjectMeta: metav1.ObjectMeta{
 				// TODO: Use a hash instead of i
-				Name:      fmt.Sprintf("%s-%d", r.NamespacedName.Name, i+1),
+				Name:      fmt.Sprintf("%s-%d", r.NamespacedName.Name, 1),
 				Namespace: r.NamespacedName.Namespace,
 			},
 			Spec: velerov1.BackupStorageLocationSpec{
 				Provider: "aws",
 				StorageType: velerov1.StorageType{
-					&velerov1.ObjectStorageLocation{
-						Bucket: noobaa.NooBaaStatus.ServicesStatus.ServiceS3.ExternalDNS[0],
-						Prefix: "velero" ,
+					ObjectStorage: &velerov1.ObjectStorageLocation{
+						Bucket: noobaa.Status.Services.ServiceS3.ExternalDNS[0],
+						Prefix: "velero",
 					},
 				},
-
-				
-				
 			},
 		}
 
@@ -99,13 +147,10 @@ func (r *VeleroReconciler) ReconcileNoobaa(log logr.Logger) (bool, error) {
 				fmt.Sprintf("performed %s on backupstoragelocation %s/%s", op, bsl.Namespace, bsl.Name),
 			)
 		}
-		
-		
+
 		//OADP creates cloud-credentials secret that points to this bucket
 
 	}
-
-
 
 	// op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, ds, func() error {
 
@@ -123,7 +168,84 @@ func (r *VeleroReconciler) ReconcileNoobaa(log logr.Logger) (bool, error) {
 	// 		fmt.Sprintf("performed %s on restic deployment %s/%s", op, ds.Namespace, ds.Name),
 	// 	)
 	// }
-		
 
 	return true, nil
 }
+
+// func (r *VeleroReconciler) ReconcileNoobaa(log logr.Logger) (bool, error) {
+// 	velero := oadpv1alpha1.Velero{}
+// 	deletePropagationBackground := metav1.DeletePropagationForeground
+// 	oadpOwnedNoobaaSystemSelector := noobaa.NooBaa{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Namespace: r.NamespacedName.Namespace,
+// 		},
+// 		TypeMeta: metav1.TypeMeta{
+// 			Kind: "NooBaa",
+// 		},
+// 	}
+// 	if !velero.Spec.Noobaa {
+// 		// if not noobaa
+// 		// Deleting existing NooBaa bsl storage
+// 		noobaaBsl := r.getNoobaaBSL()
+// 		err := r.Delete(context.Background(), &noobaaBsl, &client.DeleteOptions{
+// 			PropagationPolicy: &deletePropagationBackground,
+// 		})
+// 		if err != nil {
+// 			r.EventRecorder.Event(&noobaaBsl, corev1.EventTypeWarning, "Error deleting NooBaa BSL", "")
+// 		}
+// 		//Discover existing NooBaa systems owned by OADP operator
+// 		var thisObj *client.Object
+// 		r.Get(r.Context, client.ObjectKeyFromObject(&oadpOwnedNoobaaSystemSelector), *thisObj)
+// 		for _, expected_crd_name := range expected_crd_names {
+// 			existingCrdSelector := oadpOwnedNoobaaSystemSelector
+// 			existingCrdSelector.Name = expected_crd_name
+// 			existingCrdSelector.Labels = noobaa_labels
+// 			var existingCrd *noobaa.NooBaa
+// 			err := r.Get(r.Context, r.NamespacedName, existingCrd)
+// 			if err == nil {
+// 				// Deleting NooBaa Systems owned by OADP operator
+// 				//No errors, therefor existingCrd has the object to delete
+// 				//Delete Object\
+// 				err := r.DeleteAllOf(context.Background(), existingCrd, &client.DeleteAllOfOptions{
+// 					DeleteOptions: client.DeleteOptions{
+// 						PropagationPolicy: &deletePropagationForeground,
+// 					},
+// 				})
+// 				if err != nil {
+// 					// report error
+// 				}
+// 			}
+// 		}
+
+// 		return true, nil
+// 	}
+// 	if velero.Spec.Noobaa {
+// 		// Check if NooBaa CRDs exist
+// 		// Validate that NooBaa CRDs exist
+// 		// if NooBaa CRD exist: Discover existing NooBaa System or create new
+// 		// Waiting for NooBaa system to become ready
+// 		//   # discover routes and secrets
+// 		// Create Noobaa s3 cloud-credentials secret
+// 		//   # NooBaa Bucket + noobaa bsl creation
+
+// 	}
+
+// 	// op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, ds, func() error {
+
+// 	// })
+
+// 	// if err != nil {
+// 	// 	return false, err
+// 	// }
+
+// 	// if op == controllerutil.OperationResultCreated || op == controllerutil.OperationResultUpdated {
+// 	// 	// Trigger event to indicate restic was created or updated
+// 	// 	r.EventRecorder.Event(ds,
+// 	// 		v1.EventTypeNormal,
+// 	// 		"ResticDaemonsetReconciled",
+// 	// 		fmt.Sprintf("performed %s on restic deployment %s/%s", op, ds.Namespace, ds.Name),
+// 	// 	)
+// 	// }
+
+// 	return true, nil
+// }
--- go.mod
+++ go.mod
@@ -5,45 +5,22 @@ go 1.16
 require (
 	github.com/go-logr/logr v0.4.0
 	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/noobaa/noobaa-operator/v2 v2.0.6-0.20201215082004-c6f4a83f7d61
+	github.com/hashicorp/go-hclog v0.8.0 // indirect
+	github.com/hashicorp/go-plugin v1.0.1 // indirect
+	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
+	github.com/noobaa/noobaa-operator/v5/pkg/apis/noobaa/v1alpha1 v0.0.0-00010101000000-000000000000
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.15.0
 	github.com/openshift/api v3.9.1-0.20190924102528-32369d4db2ad+incompatible
+	github.com/spf13/cobra v1.2.1 // indirect
 	github.com/vmware-tanzu/velero v1.6.1-0.20210806003158-ed5809b7fc22 // TODO: Update this to a pinned version
-	k8s.io/api v0.22.0
+	golang.org/x/tools v0.1.5 // indirect
+	k8s.io/api v0.22.1
 	k8s.io/apiextensions-apiserver v0.21.3
-	k8s.io/apimachinery v0.22.0
-	k8s.io/client-go v12.0.0+incompatible
-	k8s.io/kube-openapi v0.0.0-20210421082810-95288971da7e
+	k8s.io/apimachinery v0.22.1
+	k8s.io/client-go v0.22.1
 	k8s.io/utils v0.0.0-20210722164352-7f3ee0f31471
-	sigs.k8s.io/controller-runtime v0.9.5
+	sigs.k8s.io/controller-runtime v0.9.6
 )
 
-replace (
-	github.com/go-openapi/spec => github.com/go-openapi/spec v0.19.3
-	github.com/mikefarah/yaml/v2 => github.com/mikefarah/yq/v4 v4.11.2
-	// github.com/openshift/api => github.com/openshift/api v0.0.0-20201203102015-275406142edb
-	github.com/openshift/api => github.com/openshift/api v0.0.0-20190924102528-32369d4db2ad
-	k8s.io/api => k8s.io/api v0.22.0
-	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.21.3
-	k8s.io/apimachinery => k8s.io/apimachinery v0.22.0
-	k8s.io/apiserver => k8s.io/apiserver v0.22.0
-	k8s.io/cli-runtime => k8s.io/cli-runtime v0.22.0
-	k8s.io/client-go => k8s.io/client-go v0.22.0
-	k8s.io/cloud-provider => k8s.io/cloud-provider v0.22.0
-	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.22.0
-	k8s.io/code-generator => k8s.io/code-generator v0.22.0
-	k8s.io/component-base => k8s.io/component-base v0.22.0
-	k8s.io/cri-api => k8s.io/cri-api v0.22.0
-	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.22.0
-	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.22.0
-	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.22.0
-	k8s.io/kube-proxy => k8s.io/kube-proxy v0.22.0
-	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.22.0
-	k8s.io/kubectl => k8s.io/kubectl v0.22.0
-	k8s.io/kubelet => k8s.io/kubelet v0.22.0
-	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.22.0
-	k8s.io/metrics => k8s.io/metrics v0.22.0
-	k8s.io/mount-utils => k8s.io/mount-utils v0.22.0
-	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.22.0
-)
+replace github.com/noobaa/noobaa-operator/v5/pkg/apis/noobaa/v1alpha1 => ./pkg/apis/noobaa/v1alpha1
