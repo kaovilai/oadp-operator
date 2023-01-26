ab9538c40c8075a66957fc64807da1b32cbdd33c
Tiger Kaovilai <tkaovila@redhat.com>
2023-01-26 16:29:38 -0500

    add default CRD validation

--- api/v1alpha1/oadp_types.go
+++ api/v1alpha1/oadp_types.go
@@ -172,12 +172,15 @@ type DataMover struct {
 	// +optional
 	CredentialName string `json:"credentialName,omitempty"`
 	// User supplied timeout to be used for VolumeSnapshotBackup and VolumeSnapshotRestore to complete, default value is 10m
+	// +kubebuilder:default="10m"
 	// +optional
 	Timeout string `json:"timeout,omitempty"`
 	// the number of batched volumeSnapshotBackups that can be inProgress at once, default value is 10
+	// +kubebuilder:default=10
 	// +optional
 	MaxConcurrentBackupVolumes *int64 `json:"maxConcurrentBackupVolumes,omitempty"`
 	// the number of batched volumeSnapshotRestores that can be inProgress at once, default value is 10
+	// +kubebuilder:default=10
 	// +optional
 	MaxConcurrentRestoreVolumes *int64 `json:"maxConcurrentRestoreVolumes,omitempty"`
 }
